6bebb7dbc424362fb2c1f590eca1dd34d003ff74
oivron <oiron@statped.no>
2020-06-12 14:18:39 +0200

    Improvements

--- music.py
+++ music.py
@@ -1,34 +1,46 @@
 """
-Author: Øivind Rønning, Statped
-Github: https://github.com/oivron/mbitutils
+Module music.
 """
 
-class __Music:
-    """Music stuff"""
 
-    def __init__(self):
-        self.DADADADUM = "DADADADUM"
-        self.ENTERTAINER = "ENTERTAINER"
-        self.PRELUDE = "PRELUDE"
-        self.ODE = "ODE"
-        self.RINGTONE = "RINGTONE"
-        self.FUNK = "FUNK"
-        self.BIRTHDAY = "BIRTHDAY"
-        self.WEDDING = "WEDDING"
-        self.FUNERAL = "FUNERAL"
-        self.PUNCHLINE = "PUNCHLINE"
-        self.PYTHON = "PYTHON"
-        self.BADDY = "BADDY"
-        self.CHASE = "CHASE"
-        self.BA_DING = "BA_DING"
-        self.WAWAWAWAA = "WAWAWAWAA"
-        self.JUMP_UP = "JUMP_UP"
-        self.JUMP_DOWN = "JUMP_DOWN"
-        self.POWER_UP = "POWER_UP"
-        self.POWER_DOWN = "POWER_DOWN"
-
-    def play(self, music):
-        """Plays a melody."""
+DADADADUM = "DADADADUM"
+ENTERTAINER = "ENTERTAINER"
+PRELUDE = "PRELUDE"
+ODE = "ODE"
+NYAN = "NYAN"
+RINGTONE = "RINGTONE"
+FUNK = "FUNK"
+BLUES = "BLUES"
+BIRTHDAY = "BIRTHDAY"
+WEDDING = "WEDDING"
+FUNERAL = "FUNERAL"
+PUNCHLINE = "PUNCHLINE"
+PYTHON = "PYTHON"
+BADDY = "BADDY"
+CHASE = "CHASE"
+BA_DING = "BA_DING"
+WAWAWAWAA = "WAWAWAWAA"
+JUMP_UP = "JUMP_UP"
+JUMP_DOWN = "JUMP_DOWN"
+POWER_UP = "POWER_UP"
+POWER_DOWN = "POWER_DOWN"
 
 
-music = __Music()
+def play(*args):
+    """Plays a melody or a tune. Either:
+    music.play(music.BIRTHDAY)
+    or one of the other built-in melodies.
+    Or:
+    music.play(tune)
+    where tune is a list of notes, its octave and its duration.
+    "C2:4" is the note C in octave number 2 to be played for a duration of 4.
+    """
+    
+
+def pitch(frequency, duration):
+    """Plays a pitch of frequency for a number of milliseconds."""
+    
+
+def stop():
+    """Stops all music playback.
+    """
--- speech.py
+++ speech.py
@@ -1,15 +1,37 @@
 """
-Author: Øivind Rønning, Statped
-Github: https://github.com/oivron/mbitutils
+Module speech.
 """
 
-class __Speech:
-    """Speech synthesis"""
 
-    def __init__(self):
-        """Init"""
-    
-    def say(self, text):
-        """Read aloud selected text."""
-    
-speech = __Speech()
+def translate(words):
+    """Translate text string to phonemes.
+    Returns a string.
+    """
+    return ""
+
+
+def  pronounce(*args):
+    """Pronounce the phonemes in a string. Either:
+    speech.pronounce(phonemes)
+    Or:
+    speech.pronounce(phonemes, pitch, speed, throat, mouth)
+    Pitch, speed, throat and mouth should have a value between 0 and 255.
+    """
+
+
+def say(*args):
+    """Say the English words in the string. Either:
+    speech.say(words)
+    Or:
+    speech.say(words, speed, pitch, throat, mouth)
+    where pitch, speed, throat and mouth should have a value between 0 and 255.
+    """
+
+
+def sing(*args):
+    """Sing the phonemes contained in a string. Either:
+    speech.sing(phonemes)
+    Or:
+    speech.sing(phonemes, pitch, speed, throat, mouth)
+    where pitch, speed, throat and mouth should have a value between 0 and 255.
+    """
